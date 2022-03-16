bde0765e9ba56415fc505bb13c7566c472f6576e
Croixiosx <adriam.webs@gmail.com>
2022-03-16 18:38:09 +0100

    added dependencies and references for jquery

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-const email = document.getElementById("loginEmail");
-const password = document.getElementById("loginPw");
-
-if($('input[name=avatarOpt]:checked' == true)){
-    console.log("Si voy");
-}
